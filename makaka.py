cc7bbb4fc3cb911debfc680038e98af040d6b4a4
PISA42 <ostslava2009@gmail.com>
2024-02-10 11:25:29 +0300

    ааххахахаха

--- makaka.py
+++ makaka.py
@@ -1,11 +1,17 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
 
 
 
 @app.route("/")
 def index():
-    return flask.render_template("imkmkndex.html", text="araaaaaaa",menu = ["fjffjfj", "slawa loxx"])
+    return render_template("index.html", text="araaaaaaa",menu = ["fjffjfj", "slawa loxx"])
+
+
+@app.route('/profile/<int:name>')
+@app.route("/profile/")
+def profile(name=""):
+    return render_template("profile.html", user=name)
 
 
 if __name__ == "__main__":

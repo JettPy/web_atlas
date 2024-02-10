899c1e8c02a83b47ad4d3b4bd4f665d012e664c1
slavikkozo4ka <ostslava2009@gmail.com>
2024-02-10 11:28:53 +0300

    яйцо собаки

---
+++ zopa.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", text="ЖООООООООООПААААААААА", menu=["1", "2", "3"])
+
+
+@app.route('/profile/<int:name>')
+@app.route("/profile/")
+def profile(name=""):
+    return render_template("profile.html", user=name)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")

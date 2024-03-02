d66dd523ada980ca04e2eac18caa55fcf712324c
PISA42 <ostslava2009@gmail.com>
2024-03-02 11:06:38 +0300

    ааххахахахаkkksfbf

--- lesson6.1.py
+++ lesson6.1.py
@@ -1,15 +1,19 @@
 from datetime import datetime
-
 from flask import Flask, render_template, url_for,request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///posts.ab'
+db = SQLAlchemy
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_keysTrue)
+    title = db.Column(db.String(100),nullabl=False)
+    content = db.Column(db.Text,nullabl=False)
+    date_posted = db.Column(db.DateTime,nullabl=False,defualt=datetime.now)
 posts = []
 
 @app.route('/blog', methods=['GET','POST'])
@@ -20,7 +24,7 @@ def blog():
         post = {'title': title, 'content': content, 'date': datetime.now()}
         posts.append(post)
         print(posts)
-    return render_template("blog.html")
+    return render_template("blog.html",posts=posts)
 
 
 if __name__ == "__main__":
